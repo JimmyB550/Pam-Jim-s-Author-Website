940fa966678bae23d7302a6816743024b36ee263
JimmyB550 <119091003+JimmyB550@users.noreply.github.com>
2023-03-19 00:22:59 -0400

    Update jWebsiteAuthor.js
    This is one of the trial blocks of code that I created to help push through for user to send email.
    It aligns with one of the lines of code that is commented out in the contacts file. (not related directly to the attempts to get the 'form' working) It was more of a last-ditch effort...not working.

--- jWebsiteAuthor.js
+++ jWebsiteAuthor.js
@@ -1,2 +1,9 @@
+function sendEmail() {
+    var email = 'bondyacc@gmail.com';
+    var subject = 'Hello';
+    var body = 'Hi there,';
+    var mailtoLink = 'mailto:' + email + '?subject=' + subject + '&body=' + body;
+    window.location.href = mailtoLink
+}
 
 
